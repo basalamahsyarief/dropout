be688fc63b0785936a7c920334b2a6e7f74438e8
basalamahsyarief <basalamahsyarief@gmail.com>
2020-09-04 14:22:02 +0700

    Add Cumulative and Singular Model

--- dropout.py
+++ dropout.py
@@ -10,18 +10,23 @@ PATH = os.path.abspath(os.path.join(__file__, "./../"))
 class DropOut:
     def __init__(self, dirpath=PATH+"/"):
         self.dirpath = dirpath
-        self.model = None
-        self.scaler = None
-        self.load_model(dirpath+'model/checkpoint_dropout.model',
-                        dirpath+'model/scaler.sav')
+        self.model_cum = joblib.load(dirpath+'model/checkpoint_dropout.sav')
+        self.model_single = joblib.load(dirpath+'model/checkpoint_dropout_per_semester')
+        self.scaler = joblib.load(dirpath+'model/scaler.sav')
 
-    def load_model(self, model_path, scaler_path):
-        self.model = joblib.load(model_path)
-        self.scaler = joblib.load(scaler_path)
-
-    def predict(self, input):
+    def predict_cum(self, input):
         input = np.array(input)
         input[1] = self.scaler.transform(input[1].reshape(1,-1))[0][0]
-        result = self.model.predict(input.reshape(1,-1))
-        proba = max(self.model.predict_proba(input.reshape(1,-1))[0])
-        return {'result' : result[0], 'probability' : proba}
+        print(input)
+        result = self.model_cum.predict(input.reshape(1,-1))
+        dic = {0 : 'Tidak Tepat Waktu', 1 : 'Tepat Waktu'}
+        proba = max(self.model_cum.predict_proba(input.reshape(1,-1))[0])
+        return {'result' : dic[result[0]], 'probability' : proba}
+
+    def predict_single(self, input):
+        input = np.array(input)
+        print(input)
+        result = self.model_single.predict(input.reshape(1,-1))
+        dic = {0 : 'Tidak Tepat Waktu', 1 : 'Tepat Waktu'}
+        proba = max(self.model_single.predict_proba(input.reshape(1,-1))[0])
+        return {'result' : dic[result[0]], 'probability' : proba}
--- route.py
+++ route.py
@@ -4,6 +4,8 @@ import json
 import os
 from flask_cors import CORS, cross_origin
 import io
+import joblib
+import numpy as np
 app = Flask(__name__, static_url_path='',
             static_folder='static',
             template_folder='templates')
@@ -13,19 +15,51 @@ APP_STATIC = os.path.join(APP_ROOT, 'static')
 cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:4100"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 genDO = DropOut()
+PATH = os.path.abspath(os.path.join(__file__, "./../"))
+dirpath = PATH+"/"
+scaler = joblib.load(dirpath+'model/scaler.sav')
+scaler_ipk = joblib.load(dirpath+'model/scaler_ipk.sav')
+scaler_ips = joblib.load(dirpath+'model/scaler_ips.sav')
+scaler_semester = joblib.load(dirpath+'model/scaler_semester.sav')
+scaler_usia = joblib.load(dirpath+'model/scaler_usia.sav')
 
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/predict_cum', methods=['POST', 'GET'])
 @cross_origin()
-def trace_bulk_number():
+def predict_cum():
+    dict_jkel = {'L': 0, 'P': 1}
+    dict_kawin = {'Belum Kawin': 0, 'Janda / Duda': 1, 'Suami/Istri Meninggal': 2, 'Sudah Kawin': 3}
     json_id = request.get_json()
-    paramDO = json_id['params']
-    if len(paramDO) != 5 :
-        data = genDO.predict(paramDO)
-    else:
-        data = {"error": "params length are not equal"}
+    umur = json_id['umur']
+    jkel = dict_jkel[json_id['jkel']]
+    ips1 = json_id['ips1']
+    ips2 = json_id['ips2']
+    ips3 = json_id['ips3']
+    ips4 = json_id['ips4']
+    ips5 = json_id['ips5']
+    data = genDO.predict_cum([jkel, umur, ips1, ips2, ips3, ips4, ips5])
     return jsonify(data)
 
+@app.route('/predict_single', methods=['POST', 'GET'])
+@cross_origin()
+def predict_single():
+    dict_jkel = {'L': 0, 'P': 1}
+    dict_masuk = {'BARU': 0, 'PINDAHAN': 1}
+    dict_jenjang = {'D3': 0, 'PR': 1, 'S1': 2, 'S2': 3}
+    dict_kawin = {'Belum Kawin': 0, 'Janda / Duda': 1, 'Suami/Istri Meninggal': 2, 'Sudah Kawin': 3}
+    json_id = request.get_json()
+    umur = json_id['umur']
+    jkel = dict_jkel[json_id['jkel']]
+    masuk = dict_masuk[json_id['masuk']]
+    jenjang = dict_jenjang[json_id['jenjang']]
+    kawin = dict_kawin[json_id['kawin']]
+    semester = scaler_semester.transform(np.array(json_id['semester']).reshape(1,-1))[0][0]
+    umur = scaler_usia.transform(np.array(json_id['umur']).reshape(1,-1))[0][0]
+    ipk = scaler_ipk.transform(np.array(json_id['ipk']).reshape(1,-1))[0][0]
+    ips = scaler_ips.transform(np.array(json_id['ips']).reshape(1,-1))[0][0]
+    is_bekerja = json_id['is_bekerja']
+    data = genDO.predict_single([semester, jkel, masuk, jenjang, umur, is_bekerja, kawin, ips, ipk])
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
